fddf137da2a7b7918e1d43f9f83e86a7a43fe1f8
Joseph Burton <josephburton06@gmail.com>
2019-05-28 16:14:37 -0500

    Update prep

--- acquire_prepare.py
+++ acquire_prepare.py
@@ -8,6 +8,10 @@ def acquire_prep():
     df.columns = df.columns.str.replace(' ', '_')
     df.rename(index=str, columns={'majorphase':'major_phase'}, inplace=True)
 
+    df.drop(columns=['client_id'], inplace=True)
+
+    df.dropna(subset=['oil_eur', 'gas_eur'], inplace=True)
+
     df.type = df.type.astype('category')
     df.status = df.status.astype('category')
     df.major_phase = df.major_phase.astype('category')
@@ -15,5 +19,10 @@ def acquire_prep():
     df.prod_method = df.prod_method.astype('category')
     df.frac_fluid_type = df.frac_fluid_type.astype('category')
     df.client_id = df.client_id.astype('category')
+
+    df = df[df.status != 'Injection']
+    df = df[df.major_phase != 'INJ']
+    df = df[df.major_phase != 'SWD']
+    df = df[df.lateral_len != 0]
 
     return df
