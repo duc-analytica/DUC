6da75745b0380e3bccab86b064140bf5cfa8cf90
Joseph Burton <josephburton06@gmail.com>
2019-05-28 14:51:04 -0500

    Update a_p acquire function

--- acquire_prepare.py
+++ acquire_prepare.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 def acquire_prep():
-    df = pd.read_excel('CapstoneData(5-28-2019).xlsx')
+    df = pd.read_excel('CapstoneData(5-28-2019).xlsx').infer_objects()
 
     df.columns = map(str.lower, df.columns)
     df.columns = df.columns.str.replace(' ', '_')
-    df.rename(index=str, columns{'majorphase':'major_phase'}, inplace=True)
+    df.rename(index=str, columns={'majorphase':'major_phase'}, inplace=True)
 
     return df
