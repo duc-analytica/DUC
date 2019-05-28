3dc5e2ae7470dde15f4122317bca9b475b6c0bfe
Joseph Burton <josephburton06@gmail.com>
2019-05-28 14:57:41 -0500

    Update a_p column types

--- acquire_prepare.py
+++ acquire_prepare.py
@@ -8,4 +8,12 @@ def acquire_prep():
     df.columns = df.columns.str.replace(' ', '_')
     df.rename(index=str, columns={'majorphase':'major_phase'}, inplace=True)
 
+    df.type = df.type.astype('category')
+    df.status = df.status.astype('category')
+    df.major_phase = df.major_phase.astype('category')
+    df.entity_reserve_category = df.entity_reserve_category.astype('category')
+    df.prod_method = df.prod_method.astype('category')
+    df.frac_fluid_type = df.frac_fluid_type.astype('category')
+    df.client_id = df.client_id.astype('category')
+
     return df
