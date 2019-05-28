e22ecdef356092c234352befa8e1ab82a850f1de
Joseph Burton <josephburton06@gmail.com>
2019-05-28 14:38:40 -0500

    Add first version of a_p

---
+++ acquire_prepare.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def acquire_prep():
+    df = pd.read_excel('CapstoneData(5-28-2019).xlsx')
+
+    df.columns = map(str.lower, df.columns)
+    df.columns = df.columns.str.replace(' ', '_')
+    df.rename(index=str, columns{'majorphase':'major_phase'})
+
+    return df
