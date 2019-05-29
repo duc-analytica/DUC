cf319ed20742ea8f9141e01c2f2c9a0d3df46588
Eric Escalante <eric.m.escalante@gmail.com>
2019-05-29 11:34:01 -0500

    cleaned up prepare

--- acquire_prepare.py
+++ acquire_prepare.py
@@ -7,75 +7,93 @@ def acquire_oil():
     '''
     return pd.read_excel('CapstoneData(5-28-2019).xlsx').infer_objects()
 
-def prep_oil(df):
-
+def clean_columns(df):
     '''
-    Lowercase all column headers, replace spaces with _'s, and add _ to majorphase
+    Function that takes the initial dataframe columns and cleans them by lowercasing, space replacing, and placing '_' in place of space
     '''
-
     df.columns = map(str.lower, df.columns)
     df.columns = df.columns.str.replace(' ', '_')
     df.rename(index=str, columns={'majorphase':'major_phase'}, inplace=True)
+    
+    return df
 
+def drop_na(df):
     '''
-    Drop erc column, drop na's from oil_eur and gas_eur
+    Function that drops null values in specific columns
     '''
-
-    df.drop(columns=['entity_reserve_category'], inplace=True)
     df.dropna(subset=['oil_eur', 'gas_eur'], inplace=True)
+    
+    return df
 
+def numeric_to_category(df):
     '''
-    Change particular columns to category type
+    Function to change selected columns into a category type
     '''
+    cols = ['type', 'status', 'major_phase', 'prod_method', 'frac_fluid_type']
+    
+    df[cols] = df[cols].astype('category')
+    
+    return df
 
-    df.type = df.type.astype('category')
-    df.status = df.status.astype('category')
-    df.major_phase = df.major_phase.astype('category')
-    df.prod_method = df.prod_method.astype('category')
-    df.frac_fluid_type = df.frac_fluid_type.astype('category')
-
+def select_rows(df):
     '''
-    Remove particular types that we are not wanting to focus on
+    Funtion to take out unwanted rows
     '''
-
     df = df[df.status != 'Injection']
     df = df[df.major_phase != 'INJ']
     df = df[df.major_phase != 'SWD']
     df = df[df.lateral_len != 0]
+    df = df[df.lateral_len < 14000]
+    
+    return df
 
+def post_year(df):
     '''
-    Remove really old data as there appears to be some date integrity issues
+    Funtion that removes all data before year 1940
     '''
-
     df = df[df['first_prod'].dt.year > 1940]
     df = df[df['last_prod'].dt.year > 1940]
+    
+    return df
 
+def feature_engineer(df):
     '''
-    Create new columns to be able to compare recovery equivalents based on type, then drop originals
+    Function that creates two new features:
+        recovery: total number of barrels of either natural gas/oil
+        recovery_per_foot: amount of recovery per foot using the pipes lateral length
+        months_active: total number of months active
+        recover_per_month: amount each API gained per month
     '''
-
     df['recovery'] = df.oil_eur + df.gas_eur/6
     df['recovery_per_foot'] = df['recovery']/df['lateral_len'] * 1000
-    df.drop(columns=['oil_eur', 'gas_eur'], inplace=True)
-
-    '''
-    Remove data integrity observation
-    '''
-
-    df = df[df.lateral_len < 14000]
-
-    '''
-    Create new column for months active, then create new column for a ratio of recovery to months active
-    '''
-
-    df[['months_active']] = df[['last_prod']]
-    df[['months_active']] = df.last_prod.dt.to_period('M') - df.first_prod.dt.to_period('M')
-    df['months_active'].astype(str).astype(int)
+    
+    df['months_active'] = (df.last_prod.dt.to_period('M') - df.first_prod.dt.to_period('M')).astype(int)
     df['months_active'].replace('0', '1', inplace=True)
 
-    df[['recovery_per_month']] = df[['recovery']]
-    df[['recovery_per_month']] = df['recovery'] / df['months_active']
-
-    df.to_csv('cleaned_oil_df.xlsx')
-
+    df['recovery_per_month'] = df.recovery / df.months_active
+    
     return df
+
+def remove_columns(df):
+    '''
+    Function that removes columns we no longer need: 'client_id' and 'entity_reserve_category'
+    '''
+    cols_to_remove = ['client_id', 'entity_reserve_category', 'oil_eur', 'gas_eur']
+    
+    df = df.drop(columns=cols_to_remove)
+    
+    return df
+
+def prep_data(df):
+    '''
+    Function that combines all functions and produces a final dataframe in a csv
+    '''
+    df = clean_columns(df)
+    df = drop_na(df)
+    df = numeric_to_category(df)
+    df = select_rows(df)
+    df = post_year(df)
+    df = feature_engineer(df)
+    df = remove_columns(df)
+    
+    return df.to_csv('cleaned_oil_df.csv', index=False)
