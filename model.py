d77c3c38b48f0684e77fc02a42336626d96f1aab
Sandy Graham <sandy.gavlick.graham@gmail.com>
2019-06-06 10:16:45 -0500

    added get_scaled_df function

--- model.py
+++ model.py
@@ -12,6 +12,19 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, median_absolute_error
 
 
+def get_scaled_df(df):
+    numerics = ['int64', 'float64', 'float']
+
+    scaled_df = df.select_dtypes(include=numerics)
+    scaled_df = scaled_df.drop(columns=['api14', 'proppant_ppf', 'frac_fluid_gpf', 
+            'gross_perfs', 'frac_stages', 'oil_gravity', 'peak_boepd', 'oil_hist', 
+            'gas_hist', 'gor_hist', 'ip90_boeqpd', 'tvd', 'sur_lat', 'sur_long', 
+            'well_id', 'mid_point_lat', 'mid_point_long', 'recovery_per_foot', 
+            'months_active', 'recovery_per_month', 'vintage', 'vintage_bin', 
+            'encoded_direction', 'encoded_frac_fluid_type', 'encoded_county', 
+            'encoded_oper', 'encoded_formation', 'encoded_sub_basin', 'encoded_lateral_class'])
+    return scaled_df
+
 def get_numeric_columns(df, skipcolumns=[]):
     #   ''' arguments - (dataframe, optional list of strings)
     #   Purpose is to return a list of numeric columns from a dataframe
