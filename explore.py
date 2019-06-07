7bb41ff192130e9f171c25985de06014e10a499e
GaryGonzenbach <gonzenbach.gary@gmail.com>
2019-06-07 11:42:13 -0500

    feature ranking:

--- explore.py
+++ explore.py
@@ -10,7 +10,9 @@ def xgb_rank(df,target_variable,feature_percent=80,mode='gain'):
     #      mode is optional,  default is 'gain' (importance),  other values are
     #       'weight'
     # '''
-    scaled_df = get_scaled_df(df)  
+
+    scaled_df = get_scaled_df(df) 
+    scaled_df = scaled_df.drop('recovery',1) 
     scaled_columns = scaled_df.columns.values.tolist()
     xgb_params = {'max_depth': 8,'seed' : 123}
     dtrain = xgb.DMatrix(scaled_df, target_variable, feature_names=scaled_df.columns.values)
@@ -18,15 +20,16 @@ def xgb_rank(df,target_variable,feature_percent=80,mode='gain'):
     importance_dict = model.get_score(importance_type=mode)
     sorted_importance_dict = sorted(importance_dict.items(), key=lambda kv: kv[1])
     importance_df = pd.DataFrame.from_dict(sorted_importance_dict)
-    importance_df.columns = ['feature',mode]   
+    importance_df.columns = ['feature',mode] 
     importance_df.sort_values(mode, inplace = True) 
     importance_df['rank'] = importance_df[mode].rank(ascending = False)
     importance_df.sort_values('rank', inplace = True) 
     importance_df.set_index('rank', inplace = True)
+    importance_df.reset_index(inplace=True) 
     importance_df[mode] = importance_df[mode].apply(lambda x: round(x, 2))
     importance_df['cum_sum'] = round(importance_df[mode].cumsum(),2)
     importance_df['cum_perc'] = round(100*importance_df.cum_sum/importance_df[mode].sum(),2)
-    importance_df.reset_index(inplace=True) 
+    
     feature_list =[] 
     for i in range((importance_df.shape[0])): 
         feature_name = importance_df.iloc[i,1].replace('scaled_','')
--- model.py
+++ model.py
@@ -16,13 +16,13 @@ def get_scaled_df(df):
     numerics = ['int64', 'float64', 'float']
 
     scaled_df = df.select_dtypes(include=numerics)
-    scaled_df = scaled_df.drop(columns=['api14', 'recovery' ,'proppant_ppf', 'frac_fluid_gpf', 
+    scaled_df = scaled_df.drop(columns=['api14', 'proppant_ppf', 'frac_fluid_gpf', 
             'gross_perfs', 'frac_stages', 'oil_gravity', 'peak_boepd', 'oil_hist', 
             'gas_hist', 'gor_hist', 'ip90_boeqpd', 'tvd', 'sur_lat', 'sur_long', 
             'well_id', 'mid_point_lat', 'mid_point_long', 'recovery_per_foot', 
             'months_active', 'recovery_per_month', 'vintage', 'vintage_bin', 
             'encoded_direction', 'encoded_frac_fluid_type', 'encoded_county', 
-            'encoded_oper', 'encoded_formation', 'encoded_lateral_class',])
+            'encoded_oper', 'encoded_formation', 'encoded_lateral_class', 'clusterid' ])
         # This should be doable in one step, but for some reason putting 'scaled_' in one line was cutting the first letter off only two column names...
     scaled_df.columns = scaled_df.columns.str.lstrip('scaled') 
     scaled_df.columns = scaled_df.columns.str.lstrip('_') 
